7919b8990ca15918584a588aebe83286afb22a89
Emmanuel Benazera <emmanuel.benazera@deepdetect.com>
2015-09-08 11:39:05 +0200

    no autosplit fallback simplification

--- miw/job.cc
+++ miw/job.cc
@@ -150,12 +150,7 @@ int job::execute(int argc, char *argv[])
 	      }
 	    else
 	      {
-		mapreduce_appbase::initialize();
-		_mrj = new mr_job(fname.c_str(), _map_tasks, _app_name, &_lf, _store_content, _compressed, _quiet);
-		_mrj->run(_nprocs,_reduce_tasks,_quiet,_output_format,j,_ndisp,_fout);
-		delete _mrj;
-		_mrj = nullptr;
-		mapreduce_appbase::deinitialize();
+		run_mr_job(fname.c_str(),j);
 	      }
 	  }
       }
