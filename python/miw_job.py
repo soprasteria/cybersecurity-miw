b83543eabb9fec05b82ed38b613afee3e91b0b2d
Emmanuel Benazera <emmanuel.benazera@deepdetect.com>
2015-09-24 17:53:21 +0200

    quiet verbosity of miw python job control class

--- python/miw_job.py
+++ python/miw_job.py
@@ -19,7 +19,7 @@ class MIWJob:
         self.miw_command = '-fnames $fnames -ofname $ofname -format_name $format_files_repo/$logfile -output_format csv -autosplit -merge_results --memory_factor $memfactor'
                 
     def run(self,miw_options):
-        print miw_options.keys()
+        #print miw_options.keys()
         #pattern = re.compile(r'\b(' + '|'.join(re.escape(key) for key in miw_options.keys()) + r')\b')
         #local_command = pattern.sub(lambda x: miw_options[x.group()], self.miw_command)
         local_command = multi_replace(self.miw_command,miw_options)
