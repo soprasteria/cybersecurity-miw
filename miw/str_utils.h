5588ae5ecc41d308caa17174d6da92efcad3cd19
Emmanuel Benazera <emmanuel.benazera@deepdetect.com>
2015-11-26 16:28:48 +0100

    various author, emails and license updates

--- miw/str_utils.h
+++ miw/str_utils.h
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2015 SopraSteria
  * All rights reserved.
- * Author: Emmanuel Benazera <beniz@droidnik.fr>
+ * Author: Emmanuel Benazera <emmanuel.benazera@deepdetect.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
