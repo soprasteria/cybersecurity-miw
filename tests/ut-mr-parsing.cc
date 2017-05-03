16b6a98cc89ce110df1b85b097e9ecb0c7d108cd
Emmanuel Benazera <beniz@droidnik.fr>
2017-05-03 13:50:22 +0200

    added match operator unit tests

--- tests/ut-mr-parsing.cc
+++ tests/ut-mr-parsing.cc
@@ -127,7 +127,7 @@ TEST(job,testVariance)
   char tmp_ouputfile[L_tmpnam];
 
   ASSERT_NE(NULL, tmpnam(tmp_ouputfile));
-
+  
   std::string arg_line = "-fnames ../data/tests/variance.log -format_name ../miw/formats/tests/variance -output_format json --map_tasks 2 -ofname ";
   arg_line.append(tmp_ouputfile);
   std::vector<std::string> args;
@@ -223,4 +223,38 @@ TEST(job,testFilter)
   ASSERT_NE(first_line.find("\"denied_count\":2"), std::string::npos);
   ASSERT_NE(first_line.find("\"ok_count\":3"), std::string::npos);
   //TODO: check on per field result
+}
+
+TEST(job,testMatch)
+{
+  job j;
+  char tmp_outputfile[L_tmpnam];
+
+  ASSERT_NE(NULL, tmpnam(tmp_outputfile));
+  std::cerr << "TMPFILE=" << tmp_outputfile << std::endl;
+  
+  std::string arg_line = "-fnames ../data/tests/matching.log -format_name ../miw/formats/tests/match -output_format json --map_tasks 2 -ofname ";
+  arg_line.append(tmp_outputfile);
+  std::vector<std::string> args;
+  log_format::tokenize(arg_line,-1,args," ","");
+  char* cargs[args.size()+1];
+  cargs[0] = "miw";
+  for (size_t i=0;i<args.size();i++)
+    cargs[i+1] = const_cast<char*>(args.at(i).c_str());
+  j.execute(args.size()+1,cargs);
+
+  std::ifstream jsonfile(tmp_outputfile);
+  if (!jsonfile.good())
+    remove(tmp_outputfile);
+  ASSERT_EQ(true, jsonfile.good());
+
+  std::string first_line;
+  std::getline(jsonfile, first_line);
+  std::string second_line;
+  std::getline(jsonfile, second_line);
+  
+  remove(tmp_outputfile);
+
+  ASSERT_NE(first_line.find("\"val\":\"OK\""), std::string::npos);
+  ASSERT_NE(second_line.find("\"val\":\"OK\""), std::string::npos);
 }
